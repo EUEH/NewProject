8b00314f76a6e38de9e43a4de2eac43b98c51f2c
顾彬 <1107593449@qq.com>
2016-12-19 10:52:50 +0800

    创建工程

---
+++ AAA/app/src/main/java/com/eueh/aaa/MainActivity.java
@@ -0,0 +1,15 @@
+package com.eueh.aaa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //申请辽宁省2017届家庭困难毕业生援助台账的资料和数据尽快发给我，截止时间是本周三（21号）中午前。
+        //如果本班没有申报者，也请QQ告诉我一声。顺祝大家考研超常发挥，考出理想的成绩！
+    }
+}
